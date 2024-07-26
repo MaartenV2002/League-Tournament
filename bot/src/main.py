f25ca151045a2610c7a010e41d3e575e23f4d7fd
MaartenV2002 <maarten.verhoeff@caiway.net>
2024-07-26 17:10:43 +0200

    Added commands

--- bot/src/main.py
+++ bot/src/main.py
@@ -1,6 +1,6 @@
 import logging
 
-import discord
+from discord.ext import commands
 
 from utils.config import Config
 
@@ -11,16 +11,21 @@ logger = logging.getLogger(__name__)
 def main():
     config = Config()
     token = config.get('discord', 'token')
+    prefix = config.get('discord', 'prefix')
     intents = config.get_intents()
-    client = discord.Client(intents=intents)
+    bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-    @client.event
+    @bot.event
     async def on_ready():
-        logger.info(f'{client.user} is connected to Discord!')
-        server_ifo = '\n    - '.join([f'{server.name} (id: {server.id})' for server in client.guilds])
+        logger.info(f'{bot.user} is connected to Discord!')
+        server_ifo = '\n    - '.join([f'{server.name} (id: {server.id})' for server in bot.guilds])
         logger.info(f'Connected to the following servers:\n    - {server_ifo}')
 
-    client.run(token)
+    @bot.command(name='ping')
+    async def ping(ctx):
+        await ctx.send('Pong!')
+
+    bot.run(token)
 
 if __name__ == "__main__":
     main()
