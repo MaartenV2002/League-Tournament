3a69f88d1ef9f36f2abc7e8462e8aa53881e270f
MaartenV2002 <maarten.verhoeff@caiway.net>
2024-07-26 16:08:18 +0200

    Setup config, Dockerized application

---
+++ src/bot/utils/config.py
@@ -0,0 +1,36 @@
+from configparser import ConfigParser
+from pathlib import Path
+
+from discord import Intents
+
+class Config:
+    def __init__(self):
+        self.config = {}
+        self.filepath = Path(__file__).resolve().parents[1] / '.env'
+        self.load()
+
+    def load(self):
+        """Load the configuration from the .env file."""
+        config = ConfigParser()
+        config.read(self.filepath)
+        self.config = {section: dict(config[section]) for section in config.sections()}
+    
+    def get(self, section, key):
+        """Get a value from the configuration."""
+        if section not in self.config:
+            raise KeyError(f'Section {section} not found in configuration')
+        if key not in self.config[section]:
+            raise KeyError(f'Key {key} not found in section {section}')
+        return self.config[section][key]
+
+    def get_intents(self):
+        """Get the intents from the configuration and return a discord.Intents object."""
+        if 'discord' not in self.config:
+            raise KeyError(f'Section discord not found in configuration. Make sure to configure the environmental variables correctly.')
+        intent_dict = {}
+        for key in self.config['discord']:
+            if key.startswith('intent_'):
+                intent_name = key.split('intent_')[-1]
+                intent_value = self.get('discord', key)
+                intent_dict[intent_name] = bool(intent_value)
+        return Intents(**intent_dict)
